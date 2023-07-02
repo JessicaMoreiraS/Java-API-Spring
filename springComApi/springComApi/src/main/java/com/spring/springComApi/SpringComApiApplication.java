c5222160a30c13f1a7228c3fd3afc125f98d79bb
JessicaMoreiraS <jessica.moreira.roso@gmail.com>
2023-07-02 18:31:05 -0300

    first commit

---
+++ springComApi/springComApi/src/main/java/com/spring/springComApi/SpringComApiApplication.java
@@ -0,0 +1,14 @@
+package com.spring.springComApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication /*biblioteca jackson-annotation em Maven Dependencies*/
+public class SpringComApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComApiApplication.class, args);
+	}
+
+}
+/*tem o webserve tomcat em Maven Dependencies*/
